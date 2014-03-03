ecf16cdebc3b7a3136f722537832a39e7846fe3e
Ajay Shah <ajaygshah@yahoo.com>
2014-03-02 23:13:01 -0600

    erasing all my work to start fresh

--- js/app.js
+++ js/app.js
@@ -11,30 +11,6 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	//Generate a random number for a new game
-  	var num = Math.floor(Math.random()*100);
-  	alert(num);
-  	var count=0;
-  	var guess;
-  	$("#userGuess").focus();
-  	$("#guessButton").click(function() {
-  		if($("#userGuess").val() == "" || $.trim($("#userGuess"))=="") {
-  			alert("Please guess.");	
-  		}
-  		else {
-  		count += 1;
-  		$("#count").text(count);
-  		guess = $("#userGuess").val();
-  		$("#guessList").append(guess + " ");
-  		$("#userGuess").focus();
-  		$("#userGuess").val("");
-
-  		return false;
-
-  		}
-
-  	})
-
 });
 
 

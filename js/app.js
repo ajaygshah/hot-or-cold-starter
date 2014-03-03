7510614e0af5251e2ada2c9f344b6efec9d3db21
Ajay Shah <ajaygshah@yahoo.com>
2014-03-02 22:48:35 -0600

    just started working on the hot or cold app.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function(){
-	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,6 +10,30 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+  	//Generate a random number for a new game
+  	var num = Math.floor(Math.random()*100);
+  	alert(num);
+  	var count=0;
+  	var guess;
+  	$("#userGuess").focus();
+  	$("#guessButton").click(function() {
+  		if($("#userGuess").val() == "" || $.trim($("#userGuess"))=="") {
+  			alert("Please guess.");	
+  		}
+  		else {
+  		count += 1;
+  		$("#count").text(count);
+  		guess = $("#userGuess").val();
+  		$("#guessList").append(guess + " ");
+  		$("#userGuess").focus();
+  		$("#userGuess").val("");
+
+  		return false;
+
+  		}
+
+  	})
 
 });
 

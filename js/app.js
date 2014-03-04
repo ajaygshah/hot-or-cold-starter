27257acebbc2e28b5abce797843f458048b04594
Ajay Shah <ajaygshah@yahoo.com>
2014-03-04 12:51:33 -0600

    starting again.

--- js/app.js
+++ js/app.js
@@ -11,6 +11,71 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	var count = 0;
+  	$("#userGuess").focus();
+ 
+  		
+ //generate a random number
+  function new_num () {
+  	var num = Math.floor(Math.random()*100);
+  	return num;
+  }
+  //......................
+  	var comp_num = new_num();
+  	alert(comp_num);
+  	click_behavior();
+
+  //To reset the game on new game click
+	function new_game () {
+		$("#count").text("0");
+		$("#guessList li").remove();
+		var comp_num = new_num();
+		alert(comp_num);
+	}  
+
+	$(".new").click(function() {
+			new_game();
+	});
+  //....................................
+
+//behavior of game once user enters a guess and clicks
+function click_behavior () {
+
+  	$("#guessButton").click(function() {
+  	
+  		var guess = +$("#userGuess").val();
+
+  			if(guess=="") {
+  				alert("please enter a number between 1 and 100.");
+  				$("#userGuess").val("").focus();
+  				return false;
+  			}
+        else if(guess>100 || guess<1){
+            alert("please enter a number between 1 and 100.");
+          $("#userGuess").val("").focus();
+          return false;
+        }
+
+  			else if(isNaN(guess)) {
+  				alert("no alpha characters. only numbers.");
+  				$("#userGuess").val("").focus();
+  				return false;
+  			}
+
+  			else {
+  				count += 1;
+  				$("#count").text(count);
+  				$("#guessList").append("<li>" + guess + " " + "</li>");
+  				$("#userGuess").val("").focus();
+
+  				return false;
+  			}
+
+
+  	})
+  }
+//..............................................
+
 });
 
 
